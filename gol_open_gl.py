e77af33df9a0732d20eebd76aae336f2043f15d8
George Merrill <gm.webdev0@gmail.com>
2018-11-05 17:17:32 -0500

    start of menu

--- gol_open_gl.py
+++ gol_open_gl.py
@@ -17,6 +17,7 @@ Any dead cell with exactly three live neighbors becomes a live cell, as if by re
 ###############################################################################################
 version 0.0.01 start of OpenGl
 version 0.0.02 cell updates and display
+version 0.0.03 start of menu
 """
 from Canvas import Canvas
 from OpenGL.GL import *
@@ -28,11 +29,8 @@ from random import randint
 screen_width = 1885
 screen_height = 1025
 cvs = Canvas(screen_width, screen_height, 'Game of Life')
-
-
 cells = [[0 for x in range(200)] for y in range(200)]
 count = 0
-
 
 def draw_message(color, coord, message):
     r,g,b = color
@@ -82,23 +80,41 @@ def cell_update():
     glutPostRedisplay()
 
 
+def draw_menu_bar():
+    glColor3f(.9, .9, .9)
+    glRecti(0, screen_height - 19, screen_width, screen_height)
+    glColor3f(0, 0, 0)
+    glRasterPos2d(2, screen_height - 15)
+    for c in 'File':
+        glutBitmapCharacter(GLUT_BITMAP_9_BY_15, ord(c))
+    glRecti(2, screen_height - 16, 11, screen_height - 17)
+
+
+def my_pass(x, y):
+    y = screen_height -y
+    glutPostRedisplay()
+
+
 def my_display():
     global cvs, cells, count
-    cvs.set_bc(1, 1, 1)
+    global red_dot
+    glutUseLayer(GLUT_NORMAL)
+    cvs.set_bc(0, 0, 0)
     cvs.clear_screen()
     glPointSize(4)
+    glColor3f(1, 1, 1)
+    glRecti(0,0,800,800)
+    glColor3f(.6, .6, 1)
+    glRecti(2,2,798,798)
     glColor3f(0, 0, 0)
-    cvs.set_window(0, 800, 0, 800)
-    cvs.set_viewport(0, 800, 0, 800)
     glBegin(GL_POINTS)
     for j in range(1,199):
         for i in range(1,199):
             if cells[j][i] == 1:
                 glVertex2i(j*4, i*4)
     glEnd()
-    cvs.set_window(500, 600, 900, 925)
-    cvs.set_viewport(200,900,800,1100)
-    draw_message((0, 0, 0),  (500, 900),f'current GENERATION: {count}')
+    draw_message((.8, .3, .8),  (500, 900),f'current GENERATION: {count}')
+    draw_menu_bar()
     cvs.swap()
     count += 1
     if count > 2000:
@@ -106,9 +122,16 @@ def my_display():
     cell_update()
 
 
-for r in range(5000):
-    x = randint(1,199)
-    y = randint(1,199)
+
+x=100
+y=100
+for r in range(500):
+    dx = randint(0,1)
+    dy = randint(0,1)
+    x,y = x + dx * 2 - 1, y + dy * 2 - 1
     cells[x][y] = 1
+
 glutDisplayFunc(my_display)
+glutPassiveMotionFunc(my_pass)
 glutMainLoop()
+
